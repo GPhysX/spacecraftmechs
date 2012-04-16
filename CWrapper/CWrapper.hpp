f9d21c89ec557a10ff9bfef01b3b69c9f32d207d
guite <guite@guite-desktop.(none)>
2012-04-16 23:28:32 +0200

    making code linux compliant

--- CWrapper/CWrapper.hpp
+++ CWrapper/CWrapper.hpp
@@ -10,6 +10,10 @@ extern "C"
         double a, u, v, w;
     };
 
+#ifndef __WIN32__
+#define __declspec(dllexport)
+#endif
+
     void *             __declspec(dllexport) SpaceCraft_Create     (double * at, double speedMax, double * ar, double angularSpeedMax);
     void               __declspec(dllexport) SpaceCraft_Release    (void * craft);
     void               __declspec(dllexport) SpaceCraft_Step       (void * craft, double delta, double rxCommand, double rzCommand, double throttle);
