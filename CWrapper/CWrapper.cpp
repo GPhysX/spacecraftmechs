b18059fb9c1c37c2c3be88b47929310a2c0c499a
Guillaume Bittoun <g.bittoun@criteo.com>
2012-04-17 10:03:26 +0200

    changing wrapper prototypes to be imported in C#

--- CWrapper/CWrapper.cpp
+++ CWrapper/CWrapper.cpp
@@ -24,33 +24,25 @@ void SpaceCraft_Step(void * craft, double delta, double rxCommand, double rzComm
     craftPtr->Step(delta, rxCommand, rzCommand, throttle);
 }
 
-struct Position SpaceCraft_GetPosition(void * craft)
+void SpaceCraft_GetPosition(void * craft, struct Position * p)
 {
     SpaceCraftMech::SpaceCraft * craftPtr = (SpaceCraftMech::SpaceCraft *)craft;
 
     MathTools::VectorD position = craftPtr->get_position();
 
-    struct Position Cposition;
-
-    Cposition.x = position.get_x();
-    Cposition.y = position.get_y();
-    Cposition.z = position.get_z();
-
-    return Cposition;
+    p->x = position.get_x();
+    p->y = position.get_y();
+    p->z = position.get_z();
 }
 
-struct Orientation SpaceCraft_GetOrientation(void * craft)
+void SpaceCraft_GetOrientation(void * craft, struct Orientation * o)
 {
     SpaceCraftMech::SpaceCraft * craftPtr = (SpaceCraftMech::SpaceCraft *)craft;
 
     MathTools::QuaternionD orientation = craftPtr->get_orientation();
 
-    struct Orientation Corientation;
-
-    Corientation.a = orientation.get_a();
-    Corientation.u = orientation.get_u();
-    Corientation.v = orientation.get_v();
-    Corientation.w = orientation.get_w();
-
-    return Corientation;
+    o->a = orientation.get_a();
+    o->u = orientation.get_u();
+    o->v = orientation.get_v();
+    o->w = orientation.get_w();
 }
--- CWrapper/CWrapper.hpp
+++ CWrapper/CWrapper.hpp
@@ -14,9 +14,9 @@ extern "C"
 #define __declspec(dllexport)
 #endif
 
-    void *             __declspec(dllexport) SpaceCraft_Create     (double * at, double speedMax, double * ar, double angularSpeedMax);
-    void               __declspec(dllexport) SpaceCraft_Release    (void * craft);
-    void               __declspec(dllexport) SpaceCraft_Step       (void * craft, double delta, double rxCommand, double rzCommand, double throttle);
-    struct Position    __declspec(dllexport) SpaceCraft_GetPosition(void * craft);
-    struct Orientation __declspec(dllexport) SpaceCraft_GetOrientation(void * craft);
+    void * __declspec(dllexport) SpaceCraft_Create         (double * at, double speedMax, double * ar, double angularSpeedMax);
+    void   __declspec(dllexport) SpaceCraft_Release        (void * craft);
+    void   __declspec(dllexport) SpaceCraft_Step           (void * craft, double delta, double rxCommand, double rzCommand, double throttle);
+    void   __declspec(dllexport) SpaceCraft_GetPosition    (void * craft, struct Position * p);
+    void   __declspec(dllexport) SpaceCraft_GetOrientation (void * craft, struct Orientation * o);
 }
